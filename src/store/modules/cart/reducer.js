cfe87daaa965a73cb5af3ccdf56bad9602947172
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-29 19:46:28 -0300

    Adicionado lógica para checar dados no estoque; Removida responsabilidade do reducer para redux saga

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -6,17 +6,12 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(
-          p => p.id === action.data.product.id
-        );
+        const {
+          data: { product },
+        } = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.data.product,
-            amount: 1,
-          });
+        if (product) {
+          draft.push(product);
         }
       });
 
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,15 +3,45 @@
 
 // o middleware é um passo a mais entre a action e o Reducer.
 
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { formatPrice } from '../../../utils/format';
+
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ data: { id } }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cartReducer.find(p => p.id === id)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn(
+      'Tentativa de comprar mais do que o permitido em estoque'
+    );
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
-//take latest: se nao terminou a chamada a API, ele vai executar somente o ultimo request disparado pelo usuário
+//takeLatest: se nao terminou a chamada a API, ele vai executar somente o ultimo request disparado pelo usuário
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
