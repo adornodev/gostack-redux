9ff45227de786fcebd46c8764906602f23004959
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-29 09:38:37 -0300

    Adicionar o gerenciamento de state do cart com immer; Adiciona lógica de amount no carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { Container, Total, ProductTable } from './styles';
 import {
@@ -7,7 +8,7 @@ import {
   MdDelete,
 } from 'react-icons/md';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +22,36 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ims=326x"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong> Tênis muito massa</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={24} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={24} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={24} color="#7159c1" />
+                  <MdDelete size={24} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={24} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={24} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
       <footer>
@@ -64,3 +64,9 @@ export default function Cart() {
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cartReducer,
+});
+
+export default connect(mapStateToProps)(Cart);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,9 +1,25 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   // action é o objeto que enviamos com dispatch
   // state é o conteúdo anterior armazenado nesse store
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.data.product];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          p => p.id === action.data.product.id
+        );
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.data.product,
+            amount: 1,
+          });
+        }
+      });
+
     default:
       return state;
   }
